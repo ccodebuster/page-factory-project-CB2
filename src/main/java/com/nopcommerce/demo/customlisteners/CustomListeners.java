22e657e58a7534107bfb39b2356fc664a34c31f2
bhav3583 <bhavesh3583@yahoo.co.in>
2023-02-05 15:06:24 +0000

    a

--- src/main/java/com/nopcommerce/demo/customlisteners/CustomListeners.java
+++ src/main/java/com/nopcommerce/demo/customlisteners/CustomListeners.java
@@ -34,6 +34,15 @@ public class CustomListeners implements ITestListener {
         test.log(Status.FAIL, "TEST FAILED IS " + iTestResult.getThrowable());
         String screenshotPath = takeScreenShot(iTestResult.getName());
         test.addScreenCaptureFromPath(screenshotPath);
+
+//        String screenshotName = Utility.takeScreenShot(iTestResult.getName());
+//        // This line required for ReportNG reports
+//        System.setProperty("org.uncommons.reportng.escape-output", "false");
+//        Reporter.log("Click to see screenshot");
+//        Reporter.log("<a target = \"_blank\" href="+screenshotName+">Screenshot</a>");
+//        Reporter.log("<br>");
+//        Reporter.log("<br>");
+//        Reporter.log("<a target = \"_blank\" href="+screenshotName+"><img src="+screenshotName+" height=200 width=200></img></a>");
     }
 
     @Override
